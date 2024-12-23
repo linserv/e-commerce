15a04deaacac075682ec59fb4e7a51262d92152c
Riyaj Pathan <rjpathan19@gmail.com>
2024-12-23 11:53:27 +0530

    FIX] website_sale_product_attachment: fixed test error by creating product record in test file instead of referencing from demo data file

--- website_sale_product_attachment/tests/test_tour.py
+++ website_sale_product_attachment/tests/test_tour.py
@@ -8,6 +8,20 @@ from odoo.tests import HttpCase, tagged
 class TestWebsiteSaleProductAttachmentTourl(HttpCase):
     def test_tour(self):
         attachment = self.env.ref("website.library_image_11")
-        product = self.env.ref("product.product_product_4_product_template")
+        product_categ_id = self.env["product.category"].create({
+            "name": "Test Category",
+            "parent_id": self.env.ref("product.product_category_all").id,
+        })
+        product = self.env["product.template"].create({
+            "name": "Test Tour Product",
+            "categ_id": product_categ_id.id,
+            "standard_price": 500.0,
+            "list_price": 750.0,
+            "detailed_type": "consu",
+            "weight": 0.01,
+            "uom_id": self.env.ref("uom.product_uom_unit").id,
+            "uom_po_id": self.env.ref("uom.product_uom_unit").id,
+            "description_sale": "160x80cm, with large legs.",
+        })
         product.website_attachment_ids = [(6, 0, [attachment.id])]
         self.start_tour("/shop", "website_sale_product_attachment_tour", login="demo")
